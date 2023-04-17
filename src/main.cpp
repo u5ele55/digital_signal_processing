7c4957798be718b890862a88b00fb6396aad37ac
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-04-17 20:33:46 +0300

    Deviation sequence calculation implemented Deviation distribution function calculation implemented

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,38 @@
 #include "signal_generator/normal_noise_signal_generator.hpp"
 #include "filtering/windowing_signal_filter.hpp"
 #include "autocorrelation/standart_autocorrelation_function.hpp"
+#include "deviation/deviation_sequence.hpp"
+#include "deviation/deviation_distribution_function.hpp"
 
 int main()
 {
     std::shared_ptr<ISignalGenerator> generator(new SimpleSignalGenerator(10, 3, 0));    
     std::shared_ptr<ISignalGenerator> noisyGenerator(new NormalNoiseSignalGenerator(10, 3, 0, 0.1));
 
+    std::shared_ptr<ISignalFilter> wFilter(new WindowingSignalFilter(3));
+
+    std::shared_ptr<IAutocorrelationFunction> autocorr(new StandartAutocorrelationFunction);
+    
+
     int N = 85;
     auto signals = generator->getSignalSequence(0, 0.1, N);
     auto noisySignals = noisyGenerator->getSignalSequence(0, 0.1, N);
-
-    std::shared_ptr<ISignalFilter> wFilter(new WindowingSignalFilter(3));
     auto filtered = wFilter->filteredSequence(noisySignals);
 
     for (int i = 0; i < N; i ++ ) {
         std::cout << signals[i] << " " << noisySignals[i] << " -> " << filtered[i] << '\n';
     }
 
-    std::shared_ptr<IAutocorrelationFunction> autocorr(new StandartAutocorrelationFunction);
-
     for(const double &v : autocorr->evaluate(filtered)) {
         std::cout << v << " ";
+    }
+    std::cout << '\n';
+
+    DeviationSequenceCalculator deviationCalc(filtered);
+    DeviationDistributionFunction devDistrF(deviationCalc.getDeviationSequence(noisySignals));
+
+    for(const auto &p : devDistrF.getFunctionValues()) {
+        std::cout << p.first << ' ' << p.second << '\n';
     }
 
     return 0;
