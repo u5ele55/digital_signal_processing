f73110f818515b6b1e7d2cedb66e8e2c65829aa0
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-04-17 18:48:05 +0300

    moved to shared_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+
 #include "signal_generator/simple_signal_generator.hpp"
 #include "signal_generator/normal_noise_signal_generator.hpp"
 #include "filtering/windowing_signal_filter.hpp"
@@ -6,31 +8,25 @@
 
 int main()
 {
-    ISignalGenerator * generator = new SimpleSignalGenerator(10, 3, 0);    
-    ISignalGenerator * noisyGenerator = new NormalNoiseSignalGenerator(10, 3, 0, 0.1);
+    std::shared_ptr<ISignalGenerator> generator(new SimpleSignalGenerator(10, 3, 0));    
+    std::shared_ptr<ISignalGenerator> noisyGenerator(new NormalNoiseSignalGenerator(10, 3, 0, 0.1));
 
     int N = 85;
     auto signals = generator->getSignalSequence(0, 0.1, N);
     auto noisySignals = noisyGenerator->getSignalSequence(0, 0.1, N);
 
-    ISignalFilter * wFilter = new WindowingSignalFilter(3);
+    std::shared_ptr<ISignalFilter> wFilter(new WindowingSignalFilter(3));
     auto filtered = wFilter->filteredSequence(noisySignals);
 
     for (int i = 0; i < N; i ++ ) {
         std::cout << signals[i] << " " << noisySignals[i] << " -> " << filtered[i] << '\n';
     }
-    
-    delete generator;
-    delete noisyGenerator;
-    delete wFilter;
 
-    IAutocorrelationFunction * autocorr = new StandartAutocorrelationFunction;
+    std::shared_ptr<IAutocorrelationFunction> autocorr(new StandartAutocorrelationFunction);
 
     for(const double &v : autocorr->evaluate(filtered)) {
         std::cout << v << " ";
     }
-
-    delete autocorr;
 
     return 0;
 }
